75a57d6f6d0c05731ea58068859b7eaef35bc29a
Jesse Baker <jesse.baker@livescore.com>
2022-02-09 13:56:27 +0100

    base test

---
+++ pages/api/end/index.tsx
@@ -0,0 +1,12 @@
+const EndPage = () => {
+  const elapsedTime = "33";
+
+  return (
+    <div>
+      <label>Elapsed Time</label>
+      <input value={elapsedTime} />
+    </div>
+  );
+};
+
+export default EndPage;
---
+++ pages/api/start/index.tsx
@@ -0,0 +1,36 @@
+import { GetServerSideProps } from "next";
+
+type Props = {};
+
+const StartPage = () => {
+  const time = Date.now();
+  const elapsedTime = 33;
+
+  const handelStartTimer = () => {
+    // ... how should this work ...
+  };
+
+  const handelStopTimer = () => {
+    // ... how should this work ...
+  };
+
+  return (
+    <div>
+      <div>Time now is {time}</div>
+
+      <div>Elapsed time: {elapsedTime}</div>
+
+      <button onClick={handelStartTimer}>Start Timer</button>
+
+      <button onClick={handelStopTimer}>Stop Timer</button>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  return {
+    props: {},
+  };
+};
+
+export default StartPage;
